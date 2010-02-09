db0468683daf51b713beda4d1cc2832e334ef51d
Sarah Smith <sarah.j.smith@nokia.com>
2010-02-09 16:54:24 +1000

    Add find & iterate benchmarks.
    This is now the baseline.  Adding balancing from here to see effect.

--- tests/benchmarks/qbsptree_perf/tst_qbsptree_perf.cpp
+++ tests/benchmarks/qbsptree_perf/tst_qbsptree_perf.cpp
@@ -53,6 +53,10 @@ public:
 private slots:
     void insert_data();
     void insert();
+    void find_data();
+    void find();
+    void iterate_data();
+    void iterate();
 };
 
 enum {
@@ -86,13 +90,11 @@ void tst_QBSPTree::insert()
     QFETCH(int, type);
 
     qsrand(314159);
-    int n = (qPow(size, 0.3333333f) + 1.0f) / 2.0f;
+    int n = (qPow((qreal)size, 0.3333333f) + 1.0f) / 2.0f;
     size = (n * 2) * (n * 2) * (n * 2);
     QVector3DArray data;
     if (type == Test_Ordered)
     {
-        qDebug() << "Ordered: adding" << n << "x" << n << "x" << n <<
-                "=" << (n*n*n) << "verts";
         for (int i = -n; i < n; ++i)
             for (int j = -n; j < n; ++j)
                 for (int k = -n; k < n; ++k)
@@ -100,15 +102,154 @@ void tst_QBSPTree::insert()
     }
     else if (type == Test_Random)
     {
-        qDebug() << "Random: adding" << size << "verts";
+        for (int i = 0; i < size; ++i)
+            data << QVector3D(mkcoord(qrand()), mkcoord(qrand()), mkcoord(qrand()));
+    }
+    QBENCHMARK {
+        QBSPTree tree(&data);
+        for (int i = 0; i < data.size(); ++i)
+        {
+            tree.insert(data.at(i), i);
+        }
+    }
+}
+
+void tst_QBSPTree::find_data()
+{
+    QTest::addColumn<int>("size");
+    QTest::addColumn<int>("type");
+
+    QByteArray name;
+    for (int size = 500; size < 20000; size += 500)
+    {
+        name = "OrderedFind--";
+        name += QString::number(size);
+        QTest::newRow(name.constData()) << size << int(Test_Ordered);
+
+        name = "RandomFind--";
+        name += QString::number(size);
+        QTest::newRow(name.constData()) << size << int(Test_Random);
+    }
+}
+
+#define mkcoord(n) (((float)(n) / (float)RAND_MAX) * 1000.0f - 500.0f)
+
+void tst_QBSPTree::find()
+{
+    QFETCH(int, size);
+    QFETCH(int, type);
+
+    qsrand(314159);
+    int n = qPow((qreal)size, 0.3333333f) / 2.0f + 1.0f;
+    size = (n * 2) * (n * 2) * (n * 2);
+    QVector3DArray data;
+    if (type == Test_Ordered)
+    {
+        for (int i = -n; i < n; ++i)
+            for (int j = -n; j < n; ++j)
+                for (int k = -n; k < n; ++k)
+                    data << QVector3D(i, j, k);
+    }
+    else if (type == Test_Random)
+    {
         for (int i = 0; i < size; ++i)
             data << QVector3D(mkcoord(qrand()), mkcoord(qrand()), mkcoord(qrand()));
     }
     QBSPTree tree(&data);
+    for (int i = 0; i < data.size(); ++i)
+    {
+        tree.insert(data.at(i), i);
+    }
     QBENCHMARK {
-        for (int i = 0; i < data.size(); ++i)
+        QBSPTree::const_iterator it = tree.constFind(data[0]);
+        QVERIFY(it != tree.constEnd());
+        QBSPTree::const_iterator it2 = tree.constFind(data[data.size()/2]);
+        QVERIFY(it2 != tree.constEnd());
+        QBSPTree::const_iterator it3 = tree.constFind(data[data.size() - 1]);
+        QVERIFY(it3 != tree.constEnd());
+    }
+}
+
+void tst_QBSPTree::iterate_data()
+{
+    QTest::addColumn<int>("size");
+    QTest::addColumn<int>("type");
+
+    QByteArray name;
+    for (int size = 500; size < 50000; size += 1000)
+    {
+        name = "OrderedIterate--";
+        name += QString::number(size);
+        QTest::newRow(name.constData()) << size << int(Test_Ordered);
+
+        name = "RandomIterate--";
+        name += QString::number(size);
+        QTest::newRow(name.constData()) << size << int(Test_Random);
+    }
+}
+
+#define mkcoord(n) (((float)(n) / (float)RAND_MAX) * 1000.0f - 500.0f)
+
+void tst_QBSPTree::iterate()
+{
+    QFETCH(int, size);
+    QFETCH(int, type);
+
+    qsrand(314159);
+    int n = qPow((qreal)size, 0.3333333f) / 2.0f;
+    size = (n * 2) * (n * 2) * (n * 2);
+    int n8 = size / 7 - 1;
+    Q_ASSERT(n8 > 0);
+    QVector3D n8v;     // have 8 copies of this one
+    QVector3DArray data;
+    if (type == Test_Ordered)
+    {
+        int tot = 0;
+        for (int i = -n; i < n; ++i)
         {
-            tree.insert(data.at(i), i);
+            for (int j = -n; j < n; ++j)
+            {
+                for (int k = -n; k < n; ++k)
+                {
+                    data << QVector3D(i, j, k);
+                    if ((tot % n8) == 0)
+                    {
+                        if (n8v.isNull())
+                            n8v = QVector3D(i, j, k);
+                        else
+                            data << n8v;
+                    }
+                    ++tot;
+                }
+            }
+        }
+    }
+    else if (type == Test_Random)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            QVector3D randvec(mkcoord(qrand()), mkcoord(qrand()), mkcoord(qrand()));
+            data << randvec;
+            if ((i % n8) == 0)
+            {
+                if (n8v.isNull())
+                    n8v = randvec;
+                else
+                    data << n8v;
+            }
+        }
+    }
+    QCOMPARE(data.size(), size+7);
+    QBSPTree tree(&data);
+    for (int i = 0; i < data.size(); ++i)
+    {
+        tree.insert(data.at(i), i);
+    }
+    QBENCHMARK {
+        QBSPTree::const_iterator it2 = tree.constBegin();
+        for ( ; it2 != tree.constEnd(); ++it2)
+        {
+            QVERIFY(it2.key() == data.at(it2.value()));
         }
     }
 }
