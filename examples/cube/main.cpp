1904546df529a1610571089d17ca06283e2280f7
Rhys Weatherley <rhys.weatherley@nokia.com>
2010-06-21 14:49:37 +1000

    Add stereo eye separation to some of the simpler examples.

--- examples/cube/main.cpp
+++ examples/cube/main.cpp
@@ -46,6 +46,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     CubeView view;
+    if (view.stereoType() != QGLView::RedCyanAnaglyph)
+        view.camera()->setEyeSeparation(0.3f);
     if (QApplication::arguments().contains("-maximize"))
         view.showMaximized();
     else if (QApplication::arguments().contains("-fullscreen"))
