chgans/qextmouse3d

commit 086372ae91023ab499f7bda829d3ff8af07783b2

Make it possible to build qglinfo without private headers

util/qglinfo/fpswidget.cpp
@@ -2,9 +2,6 @@
 
 #include <QtCore/qtimer.h>
 #include <QtCore/qdatetime.h>
-#include <QtOpenGL/private/qglextensions_p.h>
-
-#include <private/qglextensions_p.h>
 
 static GLfloat materialColor[] = { 0.1f, 0.85f, 0.25f, 1.0f };
 

util/qglinfo/qglinfo.cpp
@@ -8,7 +8,7 @@
 #include <QtCore/qtimer.h>
 #include <QtCore/qdatetime.h>
 
-#if !defined(QT_NO_EGL)
+#if !defined(QT_NO_EGL) && defined(QT_BUILD_INTERNAL)
 #include <QtGui/private/qegl_p.h>
 #include <QtGui/private/qeglproperties_p.h>
 #endif
@@ -220,7 +220,7 @@ QString QGLInfo::formatExtensions(const QByteArray& extString) const
 
 QString QGLInfo::reportEGLVersionInfo() const
 {
-#if !defined(QT_NO_EGL)
+#if !defined(QT_NO_EGL) && defined(QT_BUILD_INTERNAL)
     QString d;
     EGLDisplay dpy = eglGetCurrentDisplay();
     d += "EGL vendor string: ";
@@ -242,7 +242,7 @@ QString QGLInfo::reportEGLVersionInfo() const
 
 QString QGLInfo::reportEGLExtensionInfo() const
 {
-#if !defined(QT_NO_EGL)
+#if !defined(QT_NO_EGL) && defined(QT_BUILD_INTERNAL)
     EGLDisplay dpy = eglGetCurrentDisplay();
     QByteArray extString
         (reinterpret_cast<const char *>(eglQueryString(dpy, EGL_EXTENSIONS)));
@@ -254,7 +254,7 @@ QString QGLInfo::reportEGLExtensionInfo() const
 
 QString QGLInfo::reportEGLConfigInfo() const
 {
-#if !defined(QT_NO_EGL)
+#if !defined(QT_NO_EGL) && defined(QT_BUILD_INTERNAL)
     QString d;
     QEglProperties props;
     EGLint count = 0;

commit 0a4bbf8ca5d383a288d78e025e4182d3badb0861

Fix build error introduced by 086372ae

util/qglinfo/fpswidget.cpp
@@ -128,6 +128,10 @@ void FPSWidget::setupShaders()
 
 #endif
 
+#ifndef GL_MULTISAMPLE
+#define GL_MULTISAMPLE 0x809D
+#endif
+
 void FPSWidget::initializeGL()
 {
     glClearColor(0.9f, 0.95f, 0.8f, 1.0f);