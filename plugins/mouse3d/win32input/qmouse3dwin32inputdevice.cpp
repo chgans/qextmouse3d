0a36f2acc26a85e92303cc8d367811ca3a69f5b0
Daniel Pope <daniel.pope@nokia.com>
2010-10-19 12:18:21 +1000

    Special keys for 3d mouse.

--- plugins/mouse3d/win32input/qmouse3dwin32inputdevice.cpp
+++ plugins/mouse3d/win32input/qmouse3dwin32inputdevice.cpp
@@ -301,88 +301,88 @@ void QMouse3DWin32InputDevice::readyRead(HRAWINPUT hRawInput)
 void QMouse3DWin32InputDevice::translateMscKey(int code, bool press)
 {
 	qDebug() << "Keycode...";
-    //int qtcode = -1;
-    //switch (code) {
-    //case SPKey_Menu:
-    //    // On the SpaceNavigator, map this key to "translation lock".
-    //    if (mouseType & QMouse3DWin32InputDevice::MouseSpaceNavigator) {
-    //        if (press)
-    //            toggleFilter(QMouse3DEventProvider::Translations);
-    //        qtcode = QGL::Key_Translations;
-    //    } else {
-    //        qtcode = Qt::Key_Menu;
-    //    }
-    //    break;
+    int qtcode = -1;
+    switch (code) {
+    case V3DK_MENU:
+        // On the SpaceNavigator, map this key to "translation lock".
+        if (mouseType & QMouse3DWin32InputDevice::MouseSpaceNavigator) {
+            if (press)
+                toggleFilter(QMouse3DEventProvider::Translations);
+            qtcode = QGL::Key_Translations;
+        } else {
+            qtcode = Qt::Key_Menu;
+        }
+        break;
 
-    //case SPKey_Fit:
-    //    // On the SpaceNavigator, map this key to "rotation lock".
-    //    if (mouseType & QMouse3DWin32InputDevice::MouseSpaceNavigator) {
-    //        if (press)
-    //            toggleFilter(QMouse3DEventProvider::Rotations);
-    //        qtcode = QGL::Key_Rotations;
-    //    } else {
-    //        qtcode = QGL::Key_Fit;
-    //    }
-    //    break;
+    case V3DK_FIT:
+        // On the SpaceNavigator, map this key to "rotation lock".
+        if (mouseType & QMouse3DWin32InputDevice::MouseSpaceNavigator) {
+            if (press)
+                toggleFilter(QMouse3DEventProvider::Rotations);
+            qtcode = QGL::Key_Rotations;
+        } else {
+            qtcode = QGL::Key_Fit;
+        }
+        break;
 
-    //case SPKey_TopView:         qtcode = QGL::Key_TopView; break;
-    //case SPKey_LeftView:        qtcode = QGL::Key_LeftView; break;
-    //case SPKey_RightView:       qtcode = QGL::Key_RightView; break;
-    //case SPKey_FrontView:       qtcode = QGL::Key_FrontView; break;
-    //case SPKey_BottomView:      qtcode = QGL::Key_BottomView; break;
-    //case SPKey_BackView:        qtcode = QGL::Key_BackView; break;
-    //case SPKey_RotateCW90:      qtcode = QGL::Key_RotateCW90; break;
-    //case SPKey_RotateCCW90:     qtcode = QGL::Key_RotateCCW90; break;
-    //case SPKey_ISO1:            qtcode = QGL::Key_ISO1; break;
-    //case SPKey_ISO2:            qtcode = QGL::Key_ISO2; break;
-    //case SPKey_F1:              qtcode = QGL::Key_Button1; break;
-    //case SPKey_F2:              qtcode = QGL::Key_Button2; break;
-    //case SPKey_F3:              qtcode = QGL::Key_Button3; break;
-    //case SPKey_F4:              qtcode = QGL::Key_Button4; break;
-    //case SPKey_F5:              qtcode = QGL::Key_Button5; break;
-    //case SPKey_F6:              qtcode = QGL::Key_Button6; break;
-    //case SPKey_F7:              qtcode = QGL::Key_Button7; break;
-    //case SPKey_F8:              qtcode = QGL::Key_Button8; break;
-    //case SPKey_F9:              qtcode = QGL::Key_Button9; break;
-    //case SPKey_F10:             qtcode = QGL::Key_Button10; break;
-    //case SPKey_Escape:          qtcode = Qt::Key_Escape; break;
-    //case SPKey_Alt:             qtcode = Qt::Key_Alt; break;
-    //case SPKey_Shift:           qtcode = Qt::Key_Shift; break;
-    //case SPKey_Control:         qtcode = Qt::Key_Control; break;
+    case V3DK_TOP:         qtcode = QGL::Key_TopView; break;
+    case V3DK_LEFT:        qtcode = QGL::Key_LeftView; break;
+    case V3DK_RIGHT:       qtcode = QGL::Key_RightView; break;
+    case V3DK_FRONT:       qtcode = QGL::Key_FrontView; break;
+    case V3DK_BOTTOM:      qtcode = QGL::Key_BottomView; break;
+    case V3DK_BACK:        qtcode = QGL::Key_BackView; break;
+    case V3DK_CW:      qtcode = QGL::Key_RotateCW90; break;
+    case V3DK_CCW:     qtcode = QGL::Key_RotateCCW90; break;
+    case V3DK_ISO1:            qtcode = QGL::Key_ISO1; break;
+    case V3DK_ISO2:            qtcode = QGL::Key_ISO2; break;
+    case V3DK_1:              qtcode = QGL::Key_Button1; break;
+    case V3DK_2:              qtcode = QGL::Key_Button2; break;
+    case V3DK_3:              qtcode = QGL::Key_Button3; break;
+    case V3DK_4:              qtcode = QGL::Key_Button4; break;
+    case V3DK_5:              qtcode = QGL::Key_Button5; break;
+    case V3DK_6:              qtcode = QGL::Key_Button6; break;
+    case V3DK_7:              qtcode = QGL::Key_Button7; break;
+    case V3DK_8:              qtcode = QGL::Key_Button8; break;
+    case V3DK_9:              qtcode = QGL::Key_Button9; break;
+    case V3DK_10:             qtcode = QGL::Key_Button10; break;
+    case V3DK_ESC:          qtcode = Qt::Key_Escape; break;
+    case V3DK_ALT:             qtcode = Qt::Key_Alt; break;
+    case V3DK_SHIFT:           qtcode = Qt::Key_Shift; break;
+    case V3DK_CTRL:         qtcode = Qt::Key_Control; break;
 
-    //case SPKey_Rotation:
-    //    if (press)
-    //        toggleFilter(QMouse3DEventProvider::Rotations);
-    //    qtcode = QGL::Key_Rotations;
-    //    break;
-    //case SPKey_Pan:
-    //    if (press)
-    //        toggleFilter(QMouse3DEventProvider::Translations);
-    //    qtcode = QGL::Key_Translations;
-    //    break;
-    //case SPKey_Dominant:
-    //    if (press)
-    //        toggleFilter(QMouse3DEventProvider::DominantAxis);
-    //    qtcode = QGL::Key_DominantAxis;
-    //    break;
-    //case SPKey_IncSensitivity:
-    //    if (press)
-    //        adjustSensitivity(2.0f);
-    //    qtcode = QGL::Key_IncreaseSensitivity;
-    //    break;
-    //case SPKey_DecSensitivity:
-    //    if (press)
-    //        adjustSensitivity(0.5f);
-    //    qtcode = QGL::Key_DecreaseSensitivity;
-    //    break;
-    //default: break;
-    //}
-    //if (qtcode != -1) {
-    //    if (press)
-    //        keyPress(qtcode);
-    //    else
-    //        keyRelease(qtcode);
-    //}
+    case V3DK_ROTATE:
+        if (press)
+            toggleFilter(QMouse3DEventProvider::Rotations);
+        qtcode = QGL::Key_Rotations;
+        break;
+    case V3DK_PANZOOM:
+        if (press)
+            toggleFilter(QMouse3DEventProvider::Translations);
+        qtcode = QGL::Key_Translations;
+        break;
+    case V3DK_DOMINANT:
+        if (press)
+            toggleFilter(QMouse3DEventProvider::DominantAxis);
+        qtcode = QGL::Key_DominantAxis;
+        break;
+    case V3DK_PLUS:
+        if (press)
+            adjustSensitivity(2.0f);
+        qtcode = QGL::Key_IncreaseSensitivity;
+        break;
+    case V3DK_MINUS:
+        if (press)
+            adjustSensitivity(0.5f);
+        qtcode = QGL::Key_DecreaseSensitivity;
+        break;
+    default: break;
+    }
+    if (qtcode != -1) {
+        if (press)
+            keyPress(qtcode);
+        else
+            keyRelease(qtcode);
+    }
 }
 
 QT_END_NAMESPACE
