bc33ceeaaf2bca718cae719fee535c51f9ecf13f
Rhys Weatherley <rhys.weatherley@nokia.com>
2010-10-20 13:05:29 +1000

    Fix a crash in QGLSceneNode::deleteChild()

--- threed/scene/qglscenenode.cpp
+++ threed/scene/qglscenenode.cpp
@@ -1172,8 +1172,10 @@ void QGLSceneNode::deleteChild(QObject *object)
 void QGLSceneNode::invalidateBoundingBox() const
 {
     Q_D(const QGLSceneNode);
-    d->boxValid = false;
-    d->invalidateParentBoundingBox();
+    if (d) {    // May be null when called via deleteChild().
+        d->boxValid = false;
+        d->invalidateParentBoundingBox();
+    }
 }
 
 void QGLSceneNode::invalidateTransform() const
