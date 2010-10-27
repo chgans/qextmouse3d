3f9fdbfff9850576291eef7c2f43eac0c06212cf
Sarah Smith <sarah.j.smith@nokia.com>
2010-10-27 20:43:40 +1000

    Various optimizations.
    Replace the QImage based texture for the atlas with an FBO.  Big speed
    improvement for the thumbnailing here.
    
    Reduce the size of the sample images drastically, for quicker compiles
    and also use these when the real pictures directory is either not
    supplied or not found.
    
    Fix a bug that was stopping the exit of the app when lots of loads were
    queued up.
    
    Remove debug.

--- demos/photobrowser3d/qatlas.cpp
+++ demos/photobrowser3d/qatlas.cpp
@@ -45,53 +45,123 @@
 #include "qglpainter.h"
 #include "qgeometrydata.h"
 #include "qglmaterial.h"
+#include "qglpainter.h"
+#include "qglframebufferobjectsurface.h"
+#include "qglbuilder.h"
 
 #include <QImage>
 #include <QThread>
+#include <QGLFramebufferObject>
+#include <QGLFramebufferObjectFormat>
+#include <QCoreApplication>
+
+#ifndef GL_MULTISAMPLE
+#define GL_MULTISAMPLE  0x809D
+#endif
 
 QAtlas::QAtlas()
     : m_size(1024, 1024)
-    , m_data(new QImage(m_size, QImage::Format_ARGB32))
+    , m_data(0)
+    , m_renderTarget(0)
     , m_allocator(new QSimpleAreaAllocator(m_size))
-    , m_tex(new QGLTexture2D)
+    , m_tex(0)
     , m_material(new QGLMaterial)
-    , m_geometry(0)
+    , m_initialized(false)
+    , m_count(0)
 {
-    m_data->fill(qRgb(0, 0, 0));
-
-    // uncomment to show errors in red
-    // m_data->fill(qRgb(255, 0, 0));
-
-    m_tex->setImage(*m_data);
-    m_material->setTexture(m_tex, 1);
-    m_material->setObjectName("Atlas material");
 }
 
 QAtlas::~QAtlas()
 {
     delete m_data;
+    delete m_renderTarget;
     delete m_allocator;
     delete m_tex;
 }
 
+void QAtlas::initialize(QGLPainter *painter)
+{
+    Q_UNUSED(painter);
+    if (!m_initialized)
+    {
+        m_data = new QGLFramebufferObject(m_size);
+        m_renderTarget = new QGLFramebufferObjectSurface(m_data);
+        m_tex = QGLTexture2D::fromTextureId(m_data->texture(), m_size);
+        m_material->setTexture(m_tex, 1);
+        m_material->setObjectName("Atlas material");
+        m_initialized = true;
+    }
+}
+
+void QAtlas::paint(QGLPainter *painter)
+{
+    Q_ASSERT(QThread::currentThread() == qApp->thread());
+
+    if (m_allocationQueue.isEmpty() || painter->isPicking())
+        return;
+
+    painter->pushSurface(m_renderTarget);
+    painter->setStandardEffect(QGL::FlatReplaceTexture2D);
+    painter->projectionMatrix().push();
+    painter->modelViewMatrix().push();
+    QRect rect = painter->currentSurface()->viewportRect();
+    QMatrix4x4 proj;
+    proj.ortho(rect);
+    painter->projectionMatrix() = proj;
+    painter->modelViewMatrix().setToIdentity();
+
+    painter->update();
+
+    for (int i = 0; i < m_allocationQueue.count(); ++i)
+    {
+        QAtlasEntry entry = m_allocationQueue.at(i);
+        QRect a = entry.rect;
+        QImage image = entry.image;
+
+        if (a.left() == 0 && a.top() == 0) // first one - paint fill color
+        {
+            painter->setClearColor(Qt::red);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+
+        QGLTexture2D t;
+        t.setImage(image);
+        t.bind();
+        QVector3D va(a.left(), a.bottom()+1, 0);
+        QVector3D vb(a.right()+1, a.bottom()+1, 0);
+        QVector3D vc(a.right()+1, a.top(), 0);
+        QVector3D vd(a.left(), a.top(), 0);
+        QVector2D ta(0.0f, 0.0f);
+        QVector2D tb(1.0f, 0.0f);
+        QVector2D tc(1.0f, 1.0f);
+        QVector2D td(0.0f, 1.0f);
+        QGeometryData quad;
+        quad.setBufferStrategy(QGeometryData::KeepClientData);
+        quad.appendVertex(va, vb, vc, vd);
+        quad.appendTexCoord(ta, tb, tc, td);
+        quad.appendIndices(0, 1, 2);
+        quad.appendIndices(0, 2, 3);
+        quad.draw(painter, 0, 6);
+    }
+    m_allocationQueue.clear();
+
+    painter->popSurface();
+}
+
 QRect QAtlas::allocate(const QSize &size, const QImage &image, const QGL::IndexArray &indices)
 {
+    Q_ASSERT(QThread::currentThread() == qApp->thread());
+
     QRect a = m_allocator->allocate(size);
     if (a.isEmpty())
     {
         qWarning("QAtlas::allocate: overflowed");
         return a;
     }
-    QPainter painter;
-    painter.begin(m_data);
-    painter.drawImage(a, image);
 
-    // debug - remove me
-    // painter.setPen(QColor("orange"));
-    // painter.drawText(a, Qt::AlignCenter, "THUMB");
+    m_allocationQueue.append(QAtlasEntry(image, a));
+    ++m_count;
 
-    painter.end();
-    m_tex->setImage(*m_data);
     QRectF af(a);
     QSizeF szf(m_size);
     float l = af.left() / szf.width();
@@ -102,9 +172,6 @@ QRect QAtlas::allocate(const QSize &size, const QImage &image, const QGL::IndexA
     m_geometry.texCoord(indices.at(1), QGL::TextureCoord1) = QVector2D(r, b);
     m_geometry.texCoord(indices.at(2), QGL::TextureCoord1) = QVector2D(r, t);
     m_geometry.texCoord(indices.at(5), QGL::TextureCoord1) = QVector2D(l, t);
-
-    // uncomment to save out the atlas to an image file
-    // m_data->save("atlas_allocate.png");
     return a;
 }
 
