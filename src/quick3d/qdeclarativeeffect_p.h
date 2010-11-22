463a77fb5ded243eb375b1f9e7aa996940749324
Rhys Weatherley <rhys.weatherley@nokia.com>
2010-11-23 08:57:11 +1000

    Add "we mean it" to new private headers

--- src/quick3d/qdeclarativeeffect_p.h
+++ src/quick3d/qdeclarativeeffect_p.h
@@ -42,6 +42,17 @@
 #ifndef QDECLARATIVEEFFECT_P_H
 #define QDECLARATIVEEFFECT_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qt3dquickglobal.h"
 #include <QtCore/qobject.h>
 #include <QtCore/qurl.h>
--- src/quick3d/qdeclarativeviewport_p.h
+++ src/quick3d/qdeclarativeviewport_p.h
@@ -42,6 +42,17 @@
 #ifndef QDECLARATIVEVIEWPORT_P_H
 #define QDECLARATIVEVIEWPORT_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qt3dquickglobal.h"
 #include <QtCore/qobject.h>
 
